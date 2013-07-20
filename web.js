59948435cb6d7d24715442154088420066b90ce1
darshinit <darshini.thiagarajan@gmail.com>
2013-07-20 19:24:28 +0000

    Revert changes

--- web.js
+++ web.js
@@ -3,8 +3,11 @@ var fs = require('fs');
 var app = express();
 app.use(express.logger());
 
-fs.readFile('index.html', function (err, data) {
-  if (err) throw err;
-  data.toString('utf-8');
+app.get('/', function(request, response) {
+  response.send('Hello World!');
 });
 
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
