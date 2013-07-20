da8f9e1b01b79b98190943af4bfd2fc6db55daa6
darshinit <darshini.thiagarajan@gmail.com>
2013-07-20 19:26:44 +0000

    Restore some code

--- web.js
+++ web.js
@@ -7,6 +7,10 @@ app.get('/', function(request, response) {
   response.send('Hello World!');
 });
 
+fs.readFile('index.html', function (err, data) {
+  if (err) throw err;
+  console.log('Hello');
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
