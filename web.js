f666f7bc23f8d7fc9364f2c92d228d0c734f5d12
darshinit <darshini.thiagarajan@gmail.com>
2013-07-21 03:03:07 +0000

    styling added

--- web.js
+++ web.js
@@ -1,16 +1,15 @@
 var express = require('express');
 var fs = require('fs');
-var app = express();
-app.use(express.logger());
+var htmlfile = "index.html";
 
+//var app = express.createServer(express.logger());
+var app = express();
 app.get('/', function(request, response) {
-    fs.readFile('index.html', function (err, data) {
-   if (err) throw err;
-   response.send(data.toString('utf-8'));
-  }); 
+  var html = fs.readFileSync(htmlfile.toString());
+  response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
